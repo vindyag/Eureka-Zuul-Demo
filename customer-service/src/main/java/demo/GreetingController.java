b5d99cc46e6c3a0632fedf85cd0468aab120997c
vindyag <vindyag@99x.lk>
2022-01-04 11:06:38 +0530

    add configurations and resolve dependency issues

--- customer-service/src/main/java/demo/GreetingController.java
+++ customer-service/src/main/java/demo/GreetingController.java
@@ -1,9 +1,31 @@
 package demo;
 
+import com.netflix.discovery.EurekaClient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-public interface GreetingController {
+@RestController
+public class GreetingController {
+
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${server.port}")
+    private String portNumber;
+
+    //@Value("${spring.application.name}")
+   // private String appName;
+
     @RequestMapping("/greeting")
-    String greeting();
+    String greeting(){
+        System.out.println(portNumber + ">>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<");
+        return portNumber;
+        //return String.format("Hello from '%s'!", eurekaClient.getApplication(appName).getName());
 
+    }
 }
